07ce119df8c15e3367e7d7f96c236045d5470595
Carlo Astudillo <carloasto11@gmail.com>
2024-09-13 16:18:25 +0800

    New updates for website

--- homeindex.js
+++ homeindex.js
@@ -1,4 +1,4 @@
-var navbar = document.querySelector('.navbar');
+const navbar = document.querySelector('.navbar');
 
     function changeNavbarColor() {
       if (window.innerWidth < 992) {
@@ -11,21 +11,31 @@ var navbar = document.querySelector('.navbar');
     // Initial check when the page loads
     changeNavbarColor();
 
-    // Listen to window resize events
-    window.addEventListener('resize', changeNavbarColor);
 
-    var sticky = navbar.offsetTop;
+    function isDesktopView(){
+      return window.innerWidth > 992;
+    }
 
-    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-    function stickyNavbar() {
-      if (window.scrollY > sticky) {
-        navbar.classList.add('sticky');
-      } else {
-        navbar.classList.remove('sticky');
+    
+    function updateHeaderOnResize() {
+      var header = document.querySelector("header");
+    
+      if (!isDesktopView()) {
+        header.classList.remove("sticky"); // Remove sticky class on smaller screens
+      }
+
+      if(window.scrollY===0){
+        header.classList.remove("sticky");
       }
     }
-    
-    // When the user scrolls the page, execute stickyNavbar
-    window.onscroll = function() {
-      stickyNavbar();
-    };
+
+    window.addEventListener('resize', changeNavbarColor);
+
+
+    window.addEventListener("scroll",function(){
+      if(isDesktopView()){
+      var header = document.querySelector("header");
+      header.classList.toggle("sticky", window.scrollY>0)
+      }
+      
+    })
