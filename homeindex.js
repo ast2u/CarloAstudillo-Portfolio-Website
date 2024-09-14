e609564217feab35e405dc6941b8f50a61b41b11
Carlo Astudillo <carloasto11@gmail.com>
2024-09-15 00:00:53 +0800

    Fix and deleted some unused function

--- homeindex.js
+++ homeindex.js
@@ -2,11 +2,21 @@ let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 
+
+function isDesktopView(){
+  return window.innerWidth > 992;
+}
+
+
+
 window.addEventListener("scroll",function(){
-      if(isDesktopView()){
+
+  // Sticky function for navbar
       var header = document.querySelector("header");
       header.classList.toggle("sticky", window.scrollY>0)
-      }
+
+
+  // Navbar adding active when its on the screen
       sections.forEach(sec=>{
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
@@ -23,46 +33,3 @@ window.addEventListener("scroll",function(){
       })
       
     })
-
-
-
-
-// for testing
-
-const navbar = document.querySelector('.navbar');
-
-    function changeNavbarColor() {
-      if (window.innerWidth < 992) {
-        navbar.classList.add('navbar-mobile');
-      } else {
-        navbar.classList.remove('navbar-mobile');
-      }
-    }
-
-    // Initial check when the page loads
-    changeNavbarColor();
-
-
-    function isDesktopView(){
-      return window.innerWidth > 992;
-    }
-
-    
-    function updateHeaderOnResize() {
-      var header = document.querySelector("header");
-    
-      if (!isDesktopView()) {
-        header.classList.remove("sticky"); // Remove sticky class on smaller screens
-      }
-
-      if(window.scrollY===0){
-        header.classList.remove("sticky");
-      }
-    }
-
-
-
-    window.addEventListener('resize', changeNavbarColor);
-
-
-    
