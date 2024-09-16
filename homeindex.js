1e3902f59a244bb5f3b937c93a0dab105c0ec9ce
Carlo Astudillo <carloasto11@gmail.com>
2024-09-16 16:41:29 +0800

    added auto type js

--- homeindex.js
+++ homeindex.js
@@ -1,6 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var typed = new Typed(".auto-type", {
+    strings: ["Web Development", "Software Developer", "Cloud Enthusiast"],
+    typeSpeed: 90,
+    backSpeed: 80,
+    loop: true
+  });
+});
+
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
-
 
 
 function isDesktopView(){
@@ -33,3 +41,4 @@ window.addEventListener("scroll",function(){
       })
       
     })
+
